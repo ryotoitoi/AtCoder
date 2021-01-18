437266da3284ea77569bbe64d55b40dcaf31a07e
ryotoitoi <ryoto.tennis0813@gmail.com>
2021-01-18 23:35:09 +0900

    second commit

--- code/code.py
+++ code/code.py
@@ -8,19 +8,8 @@ sys.stdin = io.StringIO(_INPUT)
 
 n=int(input())
 a=list(map(int,input().split()))
-a.sort(reverse=True)
-l_a=[]
-l_b=[]
+from collections import Counter
 
-for i in range(n):
-      if i %2==0:
-            l_a.append(a[i])
-      else:
-            l_b.append(a[i])
-ans=sum(l_a)-sum(l_b)
-print(ans)
-      
-      
 
 
 
