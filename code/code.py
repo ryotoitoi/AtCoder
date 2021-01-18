aa08457720bd8384f6249a1d7a703d5fcf491c01
ryotoitoi <ryoto.tennis0813@gmail.com>
2021-01-18 23:41:20 +0900

    third commit

--- code/code.py
+++ code/code.py
@@ -2,14 +2,17 @@ import io
 import sys
 
 _INPUT = """\
-2 2 1 1
+6
+3 1 2 4 2 1
 """
 sys.stdin = io.StringIO(_INPUT)
 
 n=int(input())
 a=list(map(int,input().split()))
 from collections import Counter
+a_counter = Counter(a)
 
+print(a_counter)
 
 
 
