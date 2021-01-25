d576dbecfe6bd28f61c024e7adb87fcdcd37341d
ryotoitoi <ryoto.tennis0813@gmail.com>
2021-01-25 18:33:10 +0900

    solved it!

--- AtCoder/code/code.py
+++ AtCoder/code/code.py
@@ -7,72 +7,21 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 
-n,a,b,c,d = map(int,input().split())
-a,b,c,d=a-1,b-1,c-1,d-1
-s=input()
-s=[i for i in s]
-
-s[a] = "A"
-s[b] = "B"
-
-
-if c<d:
-    for i in range(n):
-        if s[d] == "B":
-            break
-        else:
-            if b+2<=len(s)-1 and s[b+2] != "#":
-                s[b+2] = "B"
-                s[b] = "."
-                b=b+2
-            elif s[b+1] != "#":
-                s[b+1] = "B"
-                s[b] = "."
-                b=b+1
-        
-    for i in range(n):
-        if s[c] == "A":
-            break
-        else:
-            if a+2<=len(s)-1 and s[a+2] != "#" and s[a+2] != "B":
-                s[a+2] = "A"
-                s[a] = "."
-                a=a+2
-            elif s[a+1] != "#" and s[a+1] != "B":
-                s[a+1] = "A"
-                s[a] = "."
-                a=a+1
-else:
-    for i in range(n):
-        if s[c] == "A":
-            break
-        else:
-            if a+2<=len(s)-1 and s[a+2] != "#":
-                s[a+2] = "A"
-                s[a] = "."
-                a=a+2
-            elif s[a+1] != "#":
-                s[a+1] = "A"
-                s[a] = "."
-                a=a+1
-        
-    for i in range(n):
-        if s[d] == "B":
-            break
-        else:
-            if b+2<=len(s)-1 and s[b+2] != "#" and s[b+2] != "A":
-                s[b+2] = "B"
-                s[b] = "."
-                b=b+2
-            elif s[b+1] != "#" and s[b+1] != "A":
-                s[b+1] = "B"
-                s[b] = "."
-                b=b+1
-    
-
-print(s)
-
-if s[c]=="A" and s[d] == "B":
-    print("Yes")
-else:
+n,a,b,c,d=map(int,input().split())
+s="0"+input()
+x=s[a:c+1]
+y=s[b:d+1]
+if "##" in s[a:c+1]:
     print("No")
+    exit()
+ 
+if "##" in s[b:d+1]:
+    print("No")
+    exit()
+ 
+if c>d:
+    if not "..." in s[b-1:d+2]:
+        print("No")
+        exit()
+    
+print("Yes")
