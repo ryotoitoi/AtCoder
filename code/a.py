d0e044403cabff636d60ebd4cf54634401395048
ryotoitoi <ryoto.tennis0813@gmail.com>
2021-02-05 00:14:09 +0900

    update

--- code/a.py
+++
@@ -1,3 +0,0 @@
-with open('./text.txt', 'r') as f:
-    for s_line in f:
-        print(s_line)
--- code/code.py
+++ code/code.py
@@ -2,30 +2,25 @@ import io
 import sys
 
 _INPUT = """\
-5 4 2
-2 1 1
-3 3 4
-
-
+15 9
+dnsusrayukuaiia
+dujrunuma
 """
 sys.stdin = io.StringIO(_INPUT)
 
-W,H,N = map(int,input().split())
-a = [input().split() for l in range(N)]
+from math import gcd
+N, M = map(int, input().split())
+S = input()
+T = input()
  
-X = Y = 0
- 
-for i in range(N):
-  if a[i][2] == "1":
-    X = max(X,int(a[i][0]))
-    
-  elif a[i][2] == "2":
-    W = min(W,int(a[i][0]))
-    
-  elif a[i][2] == "3":
-    Y = max(Y,int(a[i][1]))
-    
-  elif a[i][2] == "4":
-    H = min(H,int(a[i][1]))
+g = gcd(N, M)
+l = N * M // g
+n=N // g
+m=M // g
 
-print(max((W - X),0) * max((H - Y),0))
+for i in range(g):
+    if S[i * n] != T[i * m]:
+        print(-1)
+        exit()
+else:
+    print(l)
